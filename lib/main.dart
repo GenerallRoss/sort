4d2beed9e9decffc9765195835c803c0655440c5
Dmitry <dmitry.ananev2000@gmail.com>
2022-08-26 08:48:22 +0700

    Внесены мелкие правки

--- lib/main.dart
+++ lib/main.dart
@@ -126,7 +126,7 @@ class _MyHomePageState extends State<MyHomePage> {
                   ),
                   onPressed: () {
                     MyGraph graph = initGraph();
-                    BFSgraph(4, graph, [], []);
+                    BFSgraph(0, graph, [], []);
                   }),
             ),
           ],
